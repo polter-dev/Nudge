d4bcd270276e8e1113fcb8f6b54a85013e09f786
polter-dev <144177408+polter-dev@users.noreply.github.com>
2026-02-20 21:55:22 -0500

    set up input validation for supabase and procedural skeleton

--- src/server/api/routers/auth.ts
+++ src/server/api/routers/auth.ts
@@ -1,9 +1,30 @@
 /*
-handles the requests when a user creats an acc, logs in, logs out etc
+handles the requests when a user creates an acc, logs in, logs out etc
 using supabase
 */
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
-export const authRouter = createTRPCRouter({})
+export const authRouter = createTRPCRouter({
+    signup: publicProcedure
+    .input(z.object({ 
+        firstName: z.string(), 
+        lastName: z.string(),
+        userName: z.string(),
+        email: z.string(),
+        password: z.string()
+    }))
+    .mutation(async ({ ctx, input }) => {
+        //TODO: call supabase to create user and profile
+        return /*supa base function */({
+            data: {
+            firstName: input.firstName, 
+            lastName: input.lastName,
+            userName: input.userName,
+            email: input.email,
+            password: input.password
+            },
+        }); 
+    })
+});
